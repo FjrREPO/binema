0942fefe9057e53b9c6ee6db3d94d0738bd455c9
ahmadstiff <royanuahmad313@gmail.com>
2024-05-27 03:10:59 +0700

    hapus gambar tambah navbar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,13 @@
 import About from '@/components/client/pages/about/About'
 import React from 'react'
+import Navbar from '@/components/client/element/Navbar'
 
 function page() {
   return (
-    <div><About/></div>
+    <div>
+      <div><Navbar/></div>
+      <div><About/></div>
+    </div>
   )
 }
 
